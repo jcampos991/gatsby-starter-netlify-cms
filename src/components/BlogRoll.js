594e3d43092acc0d686bb2d1b5feaa9a40ef7bb2
Junior Campos <junior.campos@welkomtravel.com>
2019-11-12 14:21:25 -0500

    added blogRoll component to landing page

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -8,50 +8,23 @@ class BlogRoll extends React.Component {
     const { data } = this.props
     const { edges: posts } = data.allMarkdownRemark
 
+    console.log('posts :', posts);
     return (
-      <div className="columns is-multiline">
+      <div className="row">
         {posts &&
           posts.map(({ node: post }) => (
-            <div className="is-parent column is-6" key={post.id}>
-              <article
-                className={`blog-list-item tile is-child box notification ${
-                  post.frontmatter.featuredpost ? 'is-featured' : ''
-                }`}
-              >
-                <header>
-                  {post.frontmatter.featuredimage ? (
-                    <div className="featured-thumbnail">
-                      <PreviewCompatibleImage
-                        imageInfo={{
-                          image: post.frontmatter.featuredimage,
-                          alt: `featured image thumbnail for post ${post.frontmatter.title}`,
-                        }}
-                      />
+                    <div className="item col-md-4">
+                        <Link
+                            to={post.fields.slug}
+                          >
+                            {
+                              post.frontmatter.featuredimage && (
+                                <img src={post.frontmatter.featuredimage.childImageSharp.fluid.src} alt="Proyecto urban 960 Chapinero" />
+                              )
+                            }
+                            <h4>{post.frontmatter.title}</h4>
+                        </Link>
                     </div>
-                  ) : null}
-                  <p className="post-meta">
-                    <Link
-                      className="title has-text-primary is-size-4"
-                      to={post.fields.slug}
-                    >
-                      {post.frontmatter.title}
-                    </Link>
-                    <span> &bull; </span>
-                    <span className="subtitle is-size-5 is-block">
-                      {post.frontmatter.date}
-                    </span>
-                  </p>
-                </header>
-                <p>
-                  {post.excerpt}
-                  <br />
-                  <br />
-                  <Link className="button" to={post.fields.slug}>
-                    Keep Reading →
-                  </Link>
-                </p>
-              </article>
-            </div>
           ))}
       </div>
     )
@@ -85,10 +58,9 @@ export default () => (
                 title
                 templateKey
                 date(formatString: "MMMM DD, YYYY")
-                featuredpost
                 featuredimage {
                   childImageSharp {
-                    fluid(maxWidth: 120, quality: 100) {
+                    fluid {
                       ...GatsbyImageSharpFluid
                     }
                   }
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,32 +1,16 @@
 import React from 'react'
+import BlogRoll from './BlogRoll'
 
-const PostList = () => {
+const PostList = ({
+    feature3
+}) => {
     return (
         <div className="interes anoth">
             <div className="container">
                 <div className="titl">
-                    <h2>Conoce aún más...</h2>
+                    <h2>{feature3.title}</h2>
                 </div>
-                <div className="row">
-                    <div className="item col-md-4">
-                        <a href="./por-que-vivir-en-chapinero/">
-                            <img src="/img/chapineroMin.png" alt="Proyecto urban 960 Chapinero" />
-                            <h4>¿Por qué vivir en Chapinero?</h4>
-                        </a>
-                    </div>
-                    <div className="item col-md-4">
-                        <a href="./creditos-de-vivienda-bogota/">
-                            <img src="/img/credito_bogota_min.png" alt="Crédito de Vivienta Bogota" />
-                            <h4>Créditos de vivienda: Cómo Comprar un apartamento en Bogotá</h4>
-                        </a>
-                    </div>
-                    {/* <div className="item col-md-4">
-                        <a href="">
-                            <img src="/img/rectangle-copy-10.png" alt="...">
-                            <h4>Bogotá, más que un código postal</h4>
-                        </a>
-                    </div> */}
-                </div>
+                <BlogRoll />
             </div>
         </div>
     )
